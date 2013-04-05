10978a6adc63a63fd1c5237e6e04bbb0b392f770
Rafael Calleja <rafa.calleja@d-noise.net>
2013-04-05 14:57:55 +0200

    [dev]added logger for service

--- Services/SeoService.php
+++ Services/SeoService.php
@@ -1,6 +1,8 @@
 <?php
 namespace RC\PHPCRSeoBundle\Services;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
+
 use RC\PHPCRSeoBundle\Document\SeoNode;
 
 use Doctrine\ODM\PHPCR\Translation\LocaleChooser\LocaleChooser;
@@ -19,56 +21,116 @@ class SeoService {
 	 * @var \Doctrine\ODM\PHPCR\DocumentManager
 	 */
 	protected $dm;
+	
+	/**
+	 * @var null|LoggerInterface
+	 */
+	protected $logger;
 
-	public function __construct(ContainerInterface $container, $objectManagerName, LocaleChooser $localeChooser) {
+	public function __construct(ContainerInterface $container, $objectManagerName, LocaleChooser $localeChooser, LoggerInterface $logger = null ) {
 		$this->container = $container;
         $dm = $this->container->get('doctrine_phpcr')->getManager($objectManagerName);
         $this->dm = $dm->create($dm->getPhpcrSession(),  $dm->getConfiguration(), $dm->getEventManager());
         $this->dm->setLocaleChooserStrategy($localeChooser);
+        $this->logger = $logger;
 	}
 	
 	public function createSeo($basename, $seoid, $uri, $title, $keywords = false, $description = false){
-		
-		$parent = $this->dm->find(null, $basename);
-		$seoitem = $this->dm->find(null, "$basename/$seoid");
-		
-		if (!$parent) {
-			NodeHelper::createPath($this->dm->getPhpcrSession(), $basename);
+		try {
+			
 			$parent = $this->dm->find(null, $basename);
-		}
-		
-		$seoitem = ($seoitem instanceof SeoNode ) ? $seoitem : new SeoNode();
-		
-		
-		$seoitem->setParentDocument($parent);
-		$seoitem->setName($seoid);
-		$this->dm->persist($seoitem);
-		
-		$this->fixUriException();
-		$seoitem->setUri($uri);
-		$seoitem->setTitle($title);
+			$seoitem = $this->dm->find(null, "$basename/$seoid");
+			
+			if (!$parent) {
+				NodeHelper::createPath($this->dm->getPhpcrSession(), $basename);
+				$parent = $this->dm->find(null, $basename);
+			}
+			
+			$seoitem = ($seoitem instanceof SeoNode ) ? $seoitem : new SeoNode();
+			
+			
+			$seoitem->setParentDocument($parent);
+			$seoitem->setName($seoid);
+			$this->dm->persist($seoitem);
+			
+			$this->fixUriException();
+			$seoitem->setUri($uri);
+			$seoitem->setTitle($title);
+					
+			if($keywords)$seoitem->setKeywords($keywords);
+			if($description)$seoitem->setDescription($description);
+			
+			$this->dm->persist($seoitem);
+			$this->dm->flush($seoitem);
+			
+			if( $this->logger ){
+				$message = sprintf('Seo Node was created for %s/%s uri:%s, title:%s, keywords:%s, description:%s', $basename, $seoid, $uri, $title, $keywords, $description);
+				$this->logger->info($message);
+			}
+			
+			return $seoitem;
+			
+		}catch(\Exception $e){
 				
-		if($keywords)$seoitem->setKeywords($keywords);
-		if($description)$seoitem->setDescription($description);
-		
-		$this->dm->persist($seoitem);
-		$this->dm->flush($seoitem);
-		
-		return $seoitem;
+			if( $this->logger ){
+				$message = sprintf("The class %s, The method %s, Exception %s", __CLASS__, __FUNCTION__, $e->getMessage() );
+				$this->logger->error($message);
+			}
+				
+		}
 		
 	}
 	
 	public function move($source, $dest){
-		$block = $this->dm->find(null, $source);
-		$this->dm->move($block, $dest);
-		$this->dm->flush($block);
+		try {
+			
+			$block = $this->dm->find(null, $source);
+			$this->dm->move($block, $dest);
+			$this->dm->flush($block);
+			
+			if( $this->logger ){
+				$message = sprintf('Seo Node was moved From: %s TO: %s', $source, $dest);
+				$this->logger->info($message);
+			}
+			
+		}catch(\Exception $e){
+		
+			if( $this->logger ){
+				$message = sprintf("The class %s, The method %s, Exception %s", __CLASS__, __FUNCTION__, $e->getMessage() );
+				$this->logger->error($message);
+			}
+		
+		}
 	}
 	
 	public function remove($source){
-		$block = $this->dm->find(null, $source);
-		if($block instanceof SeoNode){
-			$this->dm->remove($block);
-			$this->dm->flush();
+		try{
+			
+			$block = $this->dm->find(null, $source);
+			
+			if( !$block && $this->logger ){
+				$message = sprintf("Trying to remove non-existent Seo Node source %s, class %s, method %s ", $source, __CLASS__, __FUNCTION__);
+				$this->logger->error($message);
+			}
+			
+			if($block instanceof SeoNode){
+				$this->dm->remove($block);
+				$this->dm->flush();
+				
+				if( $this->logger ){
+					$message = sprintf('Seo Node was removed %s', $source);
+					$this->logger->info($message);
+				}
+				
+			}
+			
+		}catch(\Exception $e){
+		
+			if( $this->logger ){
+				$message = sprintf("The class %s, The method %s, Exception %s", __CLASS__, __FUNCTION__, $e->getMessage() );
+				$this->logger->error($message);
+			}
+		
 		}
 	}
 	
